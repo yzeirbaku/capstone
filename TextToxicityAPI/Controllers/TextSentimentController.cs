0adc69d18057cf1dd3f2e3db31f76f44c6ab468e
yzeirbaku <38079721+yzeirbaku@users.noreply.github.com>
2019-10-29 21:23:24 +0200

    First commit

---
+++ TextToxicityAPI/Controllers/TextSentimentController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace TextToxicityAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/TextSentiment")]
+    public class TextSentimentController : Controller
+    {
+        public static string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+        // GET: api/TextSentiment
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        [HttpGet("prediction")]
+        public ActionResult Get([FromBody] string text)
+        {
+            MLContext mlContext = new MLContext();
+            TrainTestData splitDataView = LoadData(mlContext);
+
+            ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+
+            Evaluate(mlContext, model, splitDataView.TestSet);
+
+            PredictionEngine<SentimentData, SentimentPrediction> predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+            SentimentData sampleStatement = new SentimentData
+            {
+                SentimentText = text
+            };
+
+            var resultPrediction = predictionFunction.Predict(sampleStatement);
+
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
+
+            var curseCount = profanityCheck(text);
+
+            var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
+            var words = text.Split().Select(x => x.Trim(punctuation));
+
+            float wordslength = words.Count();
+            float curseRatio = curseCount / wordslength;
+
+            var returnJson = new textAnalysisResult
+            {
+                Overall = (Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative"),
+                CurseCount = curseCount,
+                CurseRatio = curseRatio
+            };
+            
+            return Ok(returnJson);
+        }
+
+        private TrainTestData LoadData(MLContext mlContext)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            return splitDataView;
+        }
+
+
+        private ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+            .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(splitTrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+
+            return model;
+
+
+        }
+
+
+        private void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            IDataView predictions = model.Transform(splitTestSet);
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+
+        }
+
+
+        // POST: api/TextSentiment
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        private int profanityCheck(string text)
+        {
+            int count = 0;
+            IList<string> censoredWords = new List<string>
+            { "bitch", "fuck", "screw", "whore", "skank"};
+
+            var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
+            var words = text.Split().Select(x => x.Trim(punctuation));
+            foreach (string a in words)
+            {
+                if (censoredWords.Contains(a))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
+
+public class textAnalysisResult
+{
+    public string Overall { get; set; }
+    public int CurseCount { get; set; }
+    public float CurseRatio { get; set; }
+}
+
+
